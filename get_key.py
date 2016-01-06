d6505193d52944b0428ca046fd52eb058a708961
Abhijeet Deshpande <avdd80@gmail.com>
2016-01-05 22:35:25 -0800

    Tkinter

--- get_key.py
+++ get_key.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 # gey_key.py
 # Get keystrokes from keyboard
-
-import pygame
-
-k = pygame.key.get_pressed()
-
-if (k == K_A):
-    print 'A'
+# respond to a key without the need to press enter
+import Tkinter as tk
+def keypress(event):
+    if event.keysym == 'Escape':
+        root.destroy()
+    x = event.char
+    if x == "w":
+        print "blaw blaw blaw"
+    elif x == "a":
+        print "blaha blaha blaha"
+    elif x == "s":
+        print "blash blash blash"
+    elif x == "d":
+        print "blad blad blad"
+    else:
+        print x
+root = tk.Tk()
+print "Press a key (Escape key to exit):"
+root.bind_all('<Key>', keypress)
+# don't show the tk window
+root.withdraw()
+root.mainloop()
