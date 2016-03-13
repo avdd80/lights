bf5d0d95e5a410cb55e71fc644474418d1e84a76
Abhijeet <avdd80@gmail.com>
2016-03-12 23:02:13 -0800

    GPIO cleanup

--- light_control_gpio.py
+++ light_control_gpio.py
@@ -48,34 +48,36 @@ def set_lamp_C (status):
     elif (status == 0):
         GPIO.output(lamp_C_gpio, GPIO.LOW)
 
+try:
+    while True:
 
-while True:
-
-    init ()
+        init ()
     
 #    print "Waiting to receive"
-    time.sleep (0.5)
-    data = udp_recv_client.recv(BUFSIZE)
-    print data
+        time.sleep (0.5)
+        data = udp_recv_client.recv(BUFSIZE)
+        print data
 
-    if (data == "space"):
-        # If all are off, turn them on
-        if (not (lamp_A_status or lamp_B_status or lamp_C_status)):
-            lamp_A_status = 1
-            lamp_B_status = 1
-            lamp_C_status = 1
-        else:        
-            lamp_A_status = 0
-            lamp_B_status = 0
-            lamp_C_status = 0
+        if (data == "space"):
+            # If all are off, turn them on
+            if (not (lamp_A_status or lamp_B_status or lamp_C_status)):
+                lamp_A_status = 1
+                lamp_B_status = 1
+                lamp_C_status = 1
+            else:        
+                lamp_A_status = 0
+                lamp_B_status = 0
+                lamp_C_status = 0
             
-    if (data == "a" or data == "A"):
-        lamp_A_staus = 1
+        if (data == "a" or data == "A"):
+            lamp_A_staus = 1
         
 
-    set_lamp_A (lamp_A_status)
-    set_lamp_B (lamp_B_status)
-    set_lamp_C (lamp_C_status)
+        set_lamp_A (lamp_A_status)
+        set_lamp_B (lamp_B_status)
+        set_lamp_C (lamp_C_status)
+    except KeyboardInterrupt:
+        GPIO.cleanup ()
 
 
 cli.close ()
